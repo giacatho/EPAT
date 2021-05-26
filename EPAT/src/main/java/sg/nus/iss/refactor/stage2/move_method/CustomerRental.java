d206c04d90471da84cca432886fe6a4bee357a3a
giacatho <giacatho@gmail.com>
2021-05-26 16:14:06 +0800

    [Refactor] add after stage2, moving method

---
+++ EPAT/src/main/java/sg/nus/iss/refactor/stage2/move_method/CustomerRental.java
@@ -0,0 +1,50 @@
+package sg.nus.iss.refactor.stage2.move_method;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerRental {
+  private String _name;
+  private List<Rental> _rentals = new ArrayList<Rental>();
+
+  public CustomerRental(String name) {
+    _name = name;
+  }
+
+  public void addRental(Rental rental) {
+    _rentals.add(rental);
+  }
+
+  public int totalRented() {
+    return _rentals.size();
+  }
+
+  public String getName() {
+    return _name;
+  }
+
+  public String statement() {
+    double totalAmount = 0;
+    int frequentRenterPoints = 0;
+    String result = "Rental Record for " + getName() + "\n";
+    
+    for (Rental each : _rentals) {
+      double thisAmount = each.getPrice();
+       // add frequent renter points
+      frequentRenterPoints++;
+
+      // add bonus for a two day new release rental
+      if ((each.getMovie().getCategory() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+        frequentRenterPoints++;
+      // show figures for this rental
+      result += each.getMovie().getTitle() + "\t" + thisAmount + "\n";
+      totalAmount += thisAmount;
+    } // end of while loop
+
+    // add footer lines
+    result += "Amount owed is " + totalAmount + "\n";
+    result += "You earned " + frequentRenterPoints + " frequent renter points";
+    return result;
+  }// end of statement method
+
+} // end of CustomerRental class
