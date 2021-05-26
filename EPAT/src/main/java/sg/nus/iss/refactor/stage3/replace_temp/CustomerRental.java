a8b627e1fad70e9de22ed947fb19dbdf5d212028
giacatho <giacatho@gmail.com>
2021-05-26 16:32:09 +0800

    [Refactor] Code after stage 3 - replace temp by query

---
+++ EPAT/src/main/java/sg/nus/iss/refactor/stage3/replace_temp/CustomerRental.java
@@ -0,0 +1,68 @@
+package sg.nus.iss.refactor.stage3.replace_temp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerRental {
+  private String _name;
+  private List<Rental> _rentals = new ArrayList<Rental>();
+
+  public CustomerRental(String name) {
+    _name = name;
+  }
+
+  public void addRental(Rental rental) {
+    _rentals.add(rental);
+  }
+
+  public int totalRented() {
+    return _rentals.size();
+  }
+
+  public String getName() {
+    return _name;
+  }
+
+  public String statement() {
+    // Refactored: Replace temp variables with queries
+    // double totalAmount = 0;
+    // int frequenRenterPoints = 0;
+    
+    String result = "Rental Record for " + getName() + "\n";
+    
+    for (Rental each : _rentals) {
+      // show figures for this rental
+      result += each.getMovie().getTitle() + "\t" + each.getPrice() + "\n";
+    } // end of while loop
+
+    // add footer lines
+    result += "Amount owed is " + getTotalAmount() + "\n";
+    result += "You earned " + getFrequentRenterPoints() + " frequent renter points";
+    return result;
+  }// end of statement method
+  
+  private int getFrequentRenterPoints() {
+    int frequentRenterPoints = 0;
+    
+    for (Rental each : _rentals) {
+      frequentRenterPoints++;
+
+      if ((each.getMovie().getCategory() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+        frequentRenterPoints++;
+    } 
+    
+    return frequentRenterPoints;
+  }
+  
+  private double getTotalAmount() {
+    double totalAmount = 0;
+    
+    for (Rental each : _rentals) {
+      double thisAmount = each.getPrice();
+      totalAmount += thisAmount;
+    } // end of while loop
+    
+    return totalAmount;
+  }
+
+} // end of CustomerRental class
---
+++ EPAT/src/main/java/sg/nus/iss/refactor/stage3/replace_temp/Movie.java
@@ -0,0 +1,27 @@
+package sg.nus.iss.refactor.stage3.replace_temp;
+
+public class Movie {
+  public static final int REGULAR = 0;
+  public static final int CHILDRENS = 1;
+  public static final int NEW_RELEASE = 2;
+
+  private String _title;
+  private int _categoryCode;
+
+  public Movie(String title, int categoryCode) {
+    _title = title;
+    _categoryCode = categoryCode;
+  }
+
+  public int getCategory() {
+    return _categoryCode;
+  }
+
+  public void setCategory(int ctg) {
+    _categoryCode = ctg;
+  }
+
+  public String getTitle() {
+    return _title;
+  }
+}
